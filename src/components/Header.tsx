00f0831bb88ab2097ded4b31e343e7a00b8c2972
Antonnri <VellikawkaTT@gmail.com>
2022-10-02 13:49:26 +0300

    README.md

---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className="header">
+        <img src="https://www.oasdom.com/wp-content/uploads/2018/01/oasdom.com-6-free-logo-design-online.jpg"/>
+    </header>
+}
+export default Header
---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className="content">
+        <div><img src="https://www.stockvault.net/data/2013/10/17/149353/thumb16.jpg" alt='logo'/></div>
+        <div><img src='https://www.stockvault.net/data/2020/02/24/273415/thumb16.jpg' alt='peoplePhoto'/></div>
+        <div>My posts
+            <div>
+                <div>New post</div>
+                <div> post 1</div>
+                <div> post 2</div>
+            </div>
+        </div>
+    </div>
+
+}
+export default Profile
