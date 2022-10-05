5b82f916ee5bccdc7d5ae721c28d79937239066f
Antonnri <VellikawkaTT@gmail.com>
2022-10-05 22:49:42 +0300

    README.md

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import Header from "../Header/Header";
+import dialogs from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props: any) => {
     return (
-        <div>
-            Dialogs
+        <div className={dialogs.dialogs}>
+            <div>
+                <div className={dialogs.dialogItems}>
+                    <div className={dialogs.dialog + ' ' + dialogs.active}>
+                        <NavLink to='/dialogs/1'> Anton</NavLink>
+                    </div>
+                    <div className={dialogs.dialog}>
+                        <NavLink to='/dialogs/2'> Katya</NavLink>
+
+                    </div>
+                    <div className={dialogs.dialog}>
+                        <NavLink to='/dialogs/3'> Sergey</NavLink>
+
+                    </div>
+                    <div className={dialogs.dialog}>
+                        <NavLink to='/dialogs/4'> Winipooh</NavLink>
+                    </div>
+                </div>
+            </div>
+            <div className={dialogs.messages}>
+                <div className={dialogs.message}>Hi</div>
+                <div className={dialogs.message}>Hy</div>
+                <div className={dialogs.message}>Very well</div>
+
+            </div>
         </div>
     )
 
